98aa18f7ee28880e07b695ad24120910ff594004
Davi Sales <davi.gomes.sales@gmail.com>
2020-03-11 19:29:49 -0300

    barra de titulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CursoComponent } from './curso/curso.component';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    CursoComponent
+    CursoComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
